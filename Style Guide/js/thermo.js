a854af5717116299814d442d75d3779630a09cfd
thomcopp <thomcopp@student.arteveldehs.be>
2017-07-03 11:14:26 +0200

    Thermometer Animation

--- Style Guide/js/thermo.js
+++ Style Guide/js/thermo.js
@@ -11,8 +11,8 @@ function thermometer() {
 	if( this.getAttribute("data-add")) {
 			thermo.classList.add(this.getAttribute("data-add"));
 			thermo.classList.add(this.getAttribute("data-height"))
-			$('#inside')[0].setAttribute("d", "M1.81,12.8V3.69A.61.61,0,0,1,3,3.69V12.8");
-			//console.log($('path')[0].attributes.d.value);
+			//$('#inside')[0].setAttribute("d", "M1.81,12.8V3.69A.61.61,0,0,1,3,3.69V12.8");
+			console.log($('path')[0].attributes.d.value);
 	}
 	if( this.getAttribute("data-remove") || this.getAttribute("data-delete")) {
 			thermo.classList.remove(this.getAttribute("data-remove"));
@@ -21,13 +21,14 @@ function thermometer() {
 			thermo.classList.remove(this.getAttribute("data-height-delete"));
 	}
 	document.getElementById('redButton').addEventListener('click',function(){
-  		document.getElementById('redanimation').beginElement();
+  		$('#inside')[0].setAttribute("d", "M1.81,12.8V3.69A.61.61,0,0,1,3,3.69V12.8").transition = "2s linear";
+;
 	},false);
 	document.getElementById('blueButton').addEventListener('click',function(){
-  		document.getElementById('blueanimation').beginElement();
+  		$('#inside')[0].setAttribute("d", "M1.81,12.8v-5A.61.61,0,0,1,3,7.78v5").transition = "2s linear";
 	},false);
 	document.getElementById('orangeButton').addEventListener('click',function(){
-  		document.getElementById('orangeanimation').beginElement();
+  		$('#inside')[0].setAttribute("d", "M1.81,12.8V5.73A.61.61,0,0,1,3,5.73V12.8").transition= "2s linear";
 	},false);
 
 	// $('#blueButton')[0].click(function(){
