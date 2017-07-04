29a0f8b9cc1741ca09e1298c3651c8eaba6a3b6a
brendero <brendero@student.arteveldehs.be>
2017-07-04 14:48:55 +0200

    v3 added

---
+++ development/Krookie/Krookie_v3/www/public/javascripts/animations.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    function loop() {
+        $('#beentje').css({top:0});
+        $('#beentje').animate ({
+            top: '+=50vw',
+        }, 2000, 'linear', function() {
+            loop();
+        });
+
+    }
+    loop();
+});
+
