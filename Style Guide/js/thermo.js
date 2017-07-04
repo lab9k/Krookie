14aef1dbdaf6fa9bf501fd7fd446aba22448b538
thomcopp <thomcopp@student.arteveldehs.be>
2017-07-04 09:37:50 +0200

    Update Thermometer

--- Style Guide/js/thermo.js
+++ Style Guide/js/thermo.js
@@ -5,19 +5,11 @@ var inside = document.getElementsByClassName("cls-2")[0];
 for (i = 0; i < btns.length; i++) {
 	btns[i].addEventListener("click", thermometer);
 }
-var boxRotationTiming = {
-  duration: 2000,
-  iterations: 1,
-  fill: "none"
-};
-
 	
 function thermometer() {
 	if( this.getAttribute("data-add")) {
 			thermo.classList.add(this.getAttribute("data-add"));
 			thermo.classList.add(this.getAttribute("data-height"))
-			//$('#inside')[0].setAttribute("d", "M1.81,12.8V3.69A.61.61,0,0,1,3,3.69V12.8");
-			console.log($('path')[0].attributes.d.value);
 	}
 	if( this.getAttribute("data-remove") || this.getAttribute("data-delete")) {
 			thermo.classList.remove(this.getAttribute("data-remove"));
@@ -25,17 +17,15 @@ function thermometer() {
 			thermo.classList.remove(this.getAttribute("data-height-remove"));
 			thermo.classList.remove(this.getAttribute("data-height-delete"));
 	}
+	// if( this )
+	// document.getElementById('redButton').addEventListener('click',function(){
+ //  		$('#inside')[0].setAttribute("d", "M1.81,12.8V3.69A.61.61,0,0,1,3,3.69V12.8");
+	// },false);
+	// document.getElementById('blueButton').addEventListener('click',function(){
+ //  		$('#inside')[0].setAttribute("d", "M1.81,12.8v-5A.61.61,0,0,1,3,7.78v5");
+	// },false);
 	document.getElementById('redButton').addEventListener('click',function(){
-<<<<<<< HEAD
-  		$('#inside')[0].setAttribute("d", "M1.81,12.8V3.69A.61.61,0,0,1,3,3.69V12.8");
-;
-	},false);
-	document.getElementById('blueButton').addEventListener('click',function(){
-  		$('#inside')[0].setAttribute("d", "M1.81,12.8v-5A.61.61,0,0,1,3,7.78v5");
-	},false);
-	document.getElementById('orangeButton').addEventListener('click',function(){
   		$('#inside')[0].setAttribute("d", "M1.81,12.8V5.73A.61.61,0,0,1,3,5.73V12.8");
-=======
   		$('#inside')[0].setAttribute("d", "M1.81,12.8V3.69A.61.61,0,0,1,3,3.69V12.8"); 
   		$('#container').removeClass();
   		$('#container').addClass('container background--red');
@@ -49,17 +39,6 @@ function thermometer() {
   		$('#inside')[0].setAttribute("d", "M1.81,12.8V5.73A.61.61,0,0,1,3,5.73V12.8");
   		$('#container').removeClass();
   		$('#container').addClass('container background--orange');
->>>>>>> 0f8f1c3d2c6dffb186ab7abffe72d706aa4fa92a
 	},false);
 
-
-	// $('#blueButton')[0].click(function(){
-	// 	$('#redanimation')[0].setAttribute("to", "M1.81,12.8v-5A.61.61,0,0,1,3,7.78v5");
-	// })
-	// $('#redButton')[0].addEventListener('click', function (){
-	// 	$('#redanimation')[0].setAttribute("d", "M1.81,12.8V3.69A.61.61,0,0,1,3,3.69V12.8");
-	// })
-	// $('#orangeButton')[0].addEventListener('click', function (){
-	// 	$('#inside')[0].setAttribute("d", "M1.81,12.8V5.73A.61.61,0,0,1,3,5.73V12.8");
-	// })
 }
